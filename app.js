9d51d7b85e2ef77def67be4ec4c2967b69723e3e
Artin-comics <91613121+Artin-comics@users.noreply.github.com>
2021-09-30 15:51:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const router = require('./src/routes/routing');
+const myErrorLogger = require('./src/utilities/errorlogger')
+const myRequestLogger = require('./src/utilities/requestlogger')
+
+
+
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, '/dist/ArtinRefactor')))
+
+
+app.use(myRequestLogger);
+app.use('/', router);
+app.use(myErrorLogger);
+
+app.listen(process.env.PORT || 8080);
+// app.listen(3000);
+console.log("Server listening in port 3000");
+
+
+module.exports = app
+
